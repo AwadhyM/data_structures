e1dd6b07a1733ef423a432de97ac2aa4c92e2bc2
Awadhy Mohammed <awadhym@outlook.com>
2026-08-29 09:32:47 +0100

    Implement and test insertion of values into BST
    Signed-off-by: Awadhy Mohammed <awadhym@outlook.com>

--- data_structures/binary_search_tree/include/binary_search_tree.hpp
+++ data_structures/binary_search_tree/include/binary_search_tree.hpp
@@ -7,9 +7,69 @@
 
 namespace ds {
 
-class BinarySearchTree {
-  public:
+template <typename T> class BinarySearchTree {
   private:
+    struct node {
+        T value;
+        node *leftChild = nullptr;
+        node *rightChild = nullptr;
+    };
+
+    node *root = nullptr;
+
+  public:
+    BinarySearchTree() = default;
+
+    bool insert(T val) { return insert(val, root); }
+
+    bool empty() { return root == nullptr; }
+
+    bool contains(const T &val) const { return contains(val, root); }
+
+    uint32_t size() { return count(root); }
+
+  private:
+    static uint32_t count(node *tree) {
+        if (tree == nullptr) {
+            return 0;
+        }
+
+        return 1 + count(tree->leftChild) + count(tree->rightChild);
+    }
+
+    static bool insert(const T &val, node *&tree) {
+        if (tree == nullptr) {
+            tree = new node;
+            tree->value = val;
+            return true;
+        }
+
+        if (tree->value == val) {
+            return false;
+        }
+
+        if (val < tree->value) {
+            return insert(val, tree->leftChild);
+        }
+
+        return insert(val, tree->rightChild);
+    }
+
+    static bool contains(const T &val, const node *tree) {
+        if (tree == nullptr) {
+            return false;
+        }
+
+        if (tree->value == val) {
+            return true;
+        }
+
+        if (val < tree->value) {
+            return contains(val, tree->leftChild);
+        }
+
+        return contains(val, tree->rightChild);
+    }
 };
 
 } // namespace ds
--- data_structures/binary_search_tree/tests/binary_search_tree_tests.cpp
+++ data_structures/binary_search_tree/tests/binary_search_tree_tests.cpp
@@ -1,3 +1,69 @@
 #include <gtest/gtest.h>
+#include "binary_search_tree.hpp"
 
-TEST(BinarySearchTreeTest, Placeholder) { SUCCEED(); }
+TEST(BinarySearchTreeTest, SuccessfullyAddToEmptyBinarySearchTree) {
+    auto bst = ds::BinarySearchTree<int>();
+    bst.insert(42);
+    EXPECT_EQ(bst.contains(42), true);
+}
+
+TEST(BinarySearchTreeTest, SuccessfullyAddLowerValueToBinaryTree) {
+    auto bst = ds::BinarySearchTree<int>();
+    bst.insert(42);
+    EXPECT_EQ(bst.contains(42), true);
+    bst.insert(36);
+    EXPECT_EQ(bst.contains(36), true);
+    bst.insert(30);
+    EXPECT_EQ(bst.contains(30), true);
+}
+
+TEST(BinarySearchTreeTest, SuccessfullyAddHigherValueToBinaryTree) {
+    auto bst = ds::BinarySearchTree<int>();
+    bst.insert(42);
+    EXPECT_EQ(bst.contains(42), true);
+    bst.insert(44);
+    EXPECT_EQ(bst.contains(44), true);
+    bst.insert(49);
+    EXPECT_EQ(bst.contains(49), true);
+}
+
+TEST(BinarySearchTreeTest, ReturnFalseForDuplicates) {
+    auto bst = ds::BinarySearchTree<int>();
+    bst.insert(42);
+    EXPECT_EQ(bst.contains(42), true);
+    EXPECT_EQ(bst.insert(42), false);
+    EXPECT_EQ(bst.size(), 1);
+}
+
+TEST(BinarySearchTreeTest, SuccessfullyAddValueToLeftChildOfRightSubtree) {
+    auto bst = ds::BinarySearchTree<int>();
+
+    bst.insert(42);
+    bst.insert(50);
+    bst.insert(45);
+    EXPECT_EQ(bst.size(), 3);
+    EXPECT_TRUE(bst.contains(45));
+}
+
+TEST(BinarySearchTreeTest, SuccessfullyAddValueToRightChildOfLeftSubtree) {
+    auto bst = ds::BinarySearchTree<int>();
+
+    bst.insert(42);
+    bst.insert(30);
+    bst.insert(35);
+
+    EXPECT_EQ(bst.size(), 3);
+    EXPECT_TRUE(bst.contains(35));
+}
+
+TEST(BinarySearchTreeTest, SuccessfullyCountBothSides) {
+    auto bst = ds::BinarySearchTree<int>();
+    bst.insert(42);
+    bst.insert(45);
+    bst.insert(36);
+    bst.insert(35);
+    bst.insert(38);
+    bst.insert(50);
+    bst.insert(45);
+    EXPECT_EQ(bst.size(), 6);
+}
